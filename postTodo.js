58de2246fda7017cecfc644fe83698192eedc942
larry <larrylinr5@gmail.com>
2022-03-21 09:46:25 +0800

    新增postTodo.js註解

--- postTodo.js
+++ postTodo.js
@@ -2,19 +2,25 @@ const { v4: uuidv4 } = require('uuid');
 const { successHandler, errorHandler } = require('./responseHandler');
 const { message } = require('./libs')
 
+/** 新增單筆代辦
+ * @param data 列表資料
+ */
 const postTodo = (data) => {
 	const { req, res, todos } = data;
+	/** http 傳來的 body 資訊 */
 	let body = "";
 
+	// 監聽 req 當 req 收到片段的 chunk 時，將片段資料加入 body 內。
 	req.on('data', chunk => {
 		body += chunk;
 	});
 
+	// 監聽 req 當 req 收完資料時，執行新增單筆代辦的功能
 	req.on('end', () => {
 		const { wrongColumn, postFail } = message
 		try {
 			const title = JSON.parse(body).title;
-			if(title !== undefined) {
+			if (title !== undefined) {
 				const todo = {
 					title,
 					'id': uuidv4(),
