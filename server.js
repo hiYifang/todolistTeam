9b4efe2ca1cda7beffe3b22850822d4defc59d72
larry <larrylinr5@gmail.com>
2022-03-21 10:37:44 +0800

    調整server.js註解

--- server.js
+++ server.js
@@ -4,40 +4,40 @@ const { errorHandler } = require('./responseHandler');
 const { getTodo, getTodos } = require('./getTodo');
 const postTodo = require('./postTodo');
 const patchTodo = require('./patchTodo');
-
-const todos = []; // 代辦清單
+/** 代辦清單 */
+const todos = []
 
 const requestListener = (req, res) => {
+  const { url, method } = req
+  const { headers, message } = libs
+  /** requestListener 資訊與清單物件 */
+  const data = {
+    /** requestListener req */
+    req,
+    /** requestListener res */
+    res,
+    /** 代辦清單 */
+    todos
+  }
 
-    const { url, method } = req
-    const { headers, message } = libs
-    const data = {
-        req,
-        res,
-        todos
-    }
-
-    if (url === "/todos" && method === "GET") {
-        getTodos(data)
-    } else if (url.startsWith("/todos/") && method === "GET") {
-        // getTodo.js
-        getTodo(data)
-    } else if (url === "/todos" && method === "POST") {
-        // postTodo.js
-        postTodo(data);
-    } else if (url === "/todos" && method === "DELETE") {
-        // deleteTodo.js
-    } else if (url.startsWith("/todos/") && method === "DELETE") {
-        // deleteTodo.js
-    } else if (url.startsWith("/todos/") && method === "PATCH") {
-        // patchTodo.js
-        patchTodo(data);
-    } else if (method === "OPTIONS") {
-        res.writeHead(200, headers);
-        res.end();
-    } else {
-        errorHandler(res, 404, message[404]);
-    }
+  if (url === "/todos" && method === "GET") {
+    getTodos(data)
+  } else if (url.startsWith("/todos/") && method === "GET") {
+    getTodo(data)
+  } else if (url === "/todos" && method === "POST") {
+    postTodo(data)
+  } else if (url === "/todos" && method === "DELETE") {
+    // deleteTodo.js
+  } else if (url.startsWith("/todos/") && method === "DELETE") {
+    // deleteTodo.js
+  } else if (url.startsWith("/todos/") && method === "PATCH") {
+    patchTodo(data)
+  } else if (method === "OPTIONS") {
+    res.writeHead(200, headers)
+    res.end()
+  } else {
+    errorHandler(res, 404, message[404])
+  }
 }
 
 const server = http.createServer(requestListener);
