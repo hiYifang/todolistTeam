922dba1a99794c71ebf1c8e3c97e34ae58a3aec0
Wu Justin <justin.cathay@gmail.com>
2022-03-23 14:43:13 +0800

    新增註解

--- server.js
+++ server.js
@@ -22,18 +22,22 @@ const requestListener = (req, res) => {
   }
 
   if (url === "/todos" && method === "GET") {
+    //取得所有待辦事項
     getTodos(data)
   } else if (url.startsWith("/todos/") && method === "GET") {
+    //取得單筆待辦事項
     getTodo(data)
   } else if (url === "/todos" && method === "POST") {
+    //新增待辦事項
     postTodo(data)
   } else if (url === "/todos" && method === "DELETE") {
-    // deleteTodo.js
+    //刪除所有待辦事項
     deleteTodos(data)
   } else if (url.startsWith("/todos/") && method === "DELETE") {
-    // deleteTodo.js
+    //刪除單筆待辦事項
     deleteTodo(data)
   } else if (url.startsWith("/todos/") && method === "PATCH") {
+    //修改單筆待辦事項
     patchTodo(data)
   } else if (method === "OPTIONS") {
     res.writeHead(200, headers)
